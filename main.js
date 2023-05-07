d43f33f9a1b12d054be37caf4b678cdc4a7d1150
Hoang Anh <hoanganhle0895@gmail.com>
2023-05-07 21:08:15 +0700

    playaudio

--- main.js
+++ main.js
@@ -2,10 +2,24 @@ var mySwiper = new Swiper('.swiper-container', {
     direction: 'vertical', // Chuyển đổi slide theo chiều dọc
     loop: true, // Lặp lại các slide
     speed: 600, // Tốc độ chuyển đổi slide
-    allowTouchMove: true,
-    touchEventsTarget: 'container', // Tên DOM container sẽ lắng nghe sự kiện touch
-    touchStartPreventDefault: false, // Không preventDefault cho sự kiện touchstart
-    touchMoveStopPropagation: true, // Ngăn chặn sự kiện touchmove lây lan sang các phần tử khác
+    on: {
+        slideChange: function () {
+            // Tìm phần tử audio cho slide hiện tại
+            var audio = this.slides[this.activeIndex].querySelector('.audio');
+            if (audio) {
+                // Nếu tìm thấy phần tử audio, thì phát nó
+                audio.play();
+            }
+
+            // Dừng phát các phần tử audio khác
+            var allAudios = document.querySelectorAll('.audio');
+            for (var i = 0; i < allAudios.length; i++) {
+                if (allAudios[i] != audio) {
+                    allAudios[i].pause();
+                }
+            }
+        }
+    }
 });
 
 var isTouching = false;
