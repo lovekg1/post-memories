657c593b8f072a96c509b782ddf145cca62b24d4
Hoang Anh <hoanganhle0895@gmail.com>
2023-05-09 20:16:43 +0700

    add API random cat

--- main.js
+++ main.js
@@ -2,24 +2,6 @@ var mySwiper = new Swiper('.swiper-container', {
     direction: 'vertical', // Chuyển đổi slide theo chiều dọc
     loop: true, // Lặp lại các slide
     speed: 600, // Tốc độ chuyển đổi slide
-    on: {
-        slideChange: function () {
-            // Tìm phần tử audio cho slide hiện tại
-            var audio = this.slides[this.activeIndex].querySelector('.audio');
-            if (audio) {
-                // Nếu tìm thấy phần tử audio, thì phát nó
-                audio.play();
-            }
-
-            // Dừng phát các phần tử audio khác
-            var allAudios = document.querySelectorAll('.audio');
-            for (var i = 0; i < allAudios.length; i++) {
-                if (allAudios[i] != audio) {
-                    allAudios[i].pause();
-                }
-            }
-        }
-    }
 });
 
 var isTouching = false;
@@ -31,3 +13,40 @@ mySwiper.on('touchStart', function () {
 mySwiper.on('touchEnd', function () {
     isTouching = false;
 });
+
+async function getRamdomCat() {
+    const res = await fetch('https://api.thecatapi.com/v1/images/search')
+    return res.json().then(data => data[0].url).catch(err => console.error(err));
+}
+
+var hasApiCalled = false
+
+mySwiper.on('slideChange', async function () {
+    if (!hasApiCalled) {
+
+        hasApiCalled = true
+        const imageUrl = await getRamdomCat()
+        const currentSlideIndex = mySwiper.activeIndex
+
+        console.log(imageUrl)
+
+        if (currentSlideIndex >= 0) {
+            // Kiểm tra xem đối tượng có tồn tại hay không trước khi truy cập đến nó
+            const prevSlideImg = mySwiper.slides[currentSlideIndex - 1]?.querySelector('.swiper-slide-img');
+            if (prevSlideImg) {
+                // Xóa hình ảnh cũ của slide trước đó
+                prevSlideImg.src = ''
+            }
+
+            // Cập nhật hình ảnh mới cho slide hiện tại
+            const currentSlideImg = mySwiper.slides[currentSlideIndex]?.querySelector('.swiper-slide-img');
+            if (currentSlideImg) {
+                currentSlideImg.src = imageUrl
+            }
+        }
+
+        setTimeout(() => {
+            hasApiCalled = false;
+        }, 1000)
+    }
+})
